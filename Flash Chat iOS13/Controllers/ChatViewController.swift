1f0a1d6d13e102db1d11f109c1906d9637b21c11
Charizard17 <3723esad@gmail.com>
2023-06-13 20:27:22 +0200

    MessageCell component created. show dummy messages on ui

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,20 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "test@mail.com", body: "Hey!"),
+        Message(sender: "test2@mail.com", body: "2023-06-13 20:25:16.291706+0200 Flash Chat iOS13[56538:4253635] [connection] nw_resolver_start_query_timer_block_invoke [C1.1] Query fired: did not receive all answers in time for www.googleapis.com:443"),
+        Message(sender: "test@mail.com", body: "What's up?"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = K.appName
         
+        title = K.appName
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -34,4 +43,18 @@ class ChatViewController: UIViewController {
             print("Error signing out: %@", signOutError)
         }
     }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+    
 }
